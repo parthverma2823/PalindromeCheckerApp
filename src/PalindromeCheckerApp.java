3c6a42eb2c8b7af851a9d7f6bddc6ac768fb3e36
parthverma2823 <parthverma719@gmail.com>
2026-03-09 14:50:12 +0530

    Made UC1 welcome message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,6 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println(" Welcome to pallindrome checker management system \n Version : 1.0 \n System initiated succesfully.");
+    }
+
 }
