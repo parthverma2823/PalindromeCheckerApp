48efaa929c618f76677cda7f8c38b26325ad76b8
parthverma2823 <parthverma719@gmail.com>
2026-03-09 15:35:53 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,28 +1,31 @@
-public class PalindromeCheckerApp {
+import java.util.Stack;
+
+public class PalindromeCheckerUC5 {
 
     public static void main(String[] args) {
 
-
+        // Original string
         String word = "madam";
 
+        // Create a stack to store characters
+        Stack<Character> stack = new Stack<>();
 
-        char[] characters = word.toCharArray();
+        // Push characters into the stack
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
 
-        int start = 0;
-        int end = characters.length - 1;
         boolean isPalindrome = true;
 
-
-        while (start < end) {
-            if (characters[start] != characters[end]) {
+        // Pop characters and compare with original string
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
 
-
+        // Print result
         if (isPalindrome) {
             System.out.println(word + " is a palindrome.");
         } else {
