faee370561c6def44cb82b1226ed54464862df7b
parthverma2823 <parthverma719@gmail.com>
2026-03-09 15:03:32 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,22 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println(" Welcome to pallindrome checker management system \n Version : 1.0 \n System initiated succesfully.");
-    }
 
+    public static void main(String[] args) {
+
+
+        String word = "madam";
+
+
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+    }
 }
